8de785ebb033ce4f35050d0b4caa70af2de6bbad
KrostyGamer <usuario8@factum.es>
2021-11-29 11:31:59 -0600

    creacion

---
+++ src/main/java/com/example/Posicion/PosicionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Posicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosicionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosicionApplication.class, args);
+	}
+
+}
